f4a410dd433d5c297c55117e26a48f7153c245cd
Max Cohen <maxcoh3n@gmail.com>
2026-05-12 10:19:57 -0700

    fix bugs for /bets with username option

--- src/slashCommandHandlers/betsHandler.ts
+++ src/slashCommandHandlers/betsHandler.ts
@@ -1,8 +1,13 @@
 import { ChatInputCommandInteraction } from "discord.js";
 
 import { AllBetsFooterMessage, UnsettledBetsMsg } from "../common/constants";
-import { betToString, fiendWagerToString } from "../common/util";
 import {
+  betToString,
+  fiendWagerToString,
+  getServerNickname,
+} from "../common/util";
+import {
+  getFiend,
   getFiendWagersByBet,
   getFiendWagersByBetAndUser,
   getUnsettledBets,
@@ -12,23 +17,30 @@ import {
 export default async function HandleBets(
   interaction: ChatInputCommandInteraction,
 ) {
-  const userOption = interaction.options.getUser('user');
+  const userOption = interaction.options.getUser("user");
 
   if (userOption) {
     const userId = userOption.id;
+    const fiend = getFiend(userId);
+    const username = fiend
+      ? fiend.name
+      : await getServerNickname(userOption, interaction);
     const unsettledBets = getUnsettledBetsByUser(userId);
 
     if (unsettledBets.length === 0) {
-      await interaction.reply(`No unsettled bets found for ${userOption.username}.`);
+      await interaction.reply(`No unsettled bets found for ${username}.`);
       return;
     }
 
     const betList = unsettledBets
-      .map((bet) => betToString(bet) + "\n" + formatFiendWagersForUser(bet.id, userId))
+      .map(
+        (bet) =>
+          betToString(bet) + "\n" + formatFiendWagersForUser(bet.id, userId),
+      )
       .join("\n");
 
     await interaction.reply(
-      `Unsettled bets for ${userOption.username}:\n${betList}${AllBetsFooterMessage}`,
+      `${UnsettledBetsMsg} for ${username}:\n${betList}${AllBetsFooterMessage}`,
     );
   } else {
     const unsettledBets = getUnsettledBets();
